cd5d769282931e50cfb1e8bd66ad9f40cf250d4a
Maruf Ahmed <marufsarkar512@gmail.com>
2023-07-27 19:24:01 +0600

    toggle betwin images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
+import { useState } from "react";
 
 const anime = {
     name: "One Punch Man",
     imageUrl:
         "https://tds-images.thedailystar.net/sites/default/files/styles/very_big_1/public/images/2023/01/31/one-punch-man.jpg",
+    punch: "https://cdn.gfinityesports.com/images/ncavvykf/gfinityesports/a0bf8d6c51ac2a45a2bfd5f60d15ab45f6783605-1920x1080.jpg?rect=0,60,1920,960&w=1200&h=600&auto=format",
 };
 
 function App() {
     return (
         <div>
             <br />
-            <h1>Hello World</h1>
-            <MyButton />
+            <h1>
+                <u>World of Anime</u>
+            </h1>
+            <br />
             <Profile />
         </div>
     );
@@ -20,24 +24,22 @@ function App() {
 export default App;
 
 const Profile = () => {
+    const [punch, setPunch] = useState(true);
+
+    function handelClick() {
+        setPunch(!punch);
+    }
+
     return (
         <div>
             <h1>{anime.name}</h1>
+            <button onClick={handelClick}>Just a ragular punch</button>
+            <br />
             <img
                 className="avatar"
-                src={anime.imageUrl}
+                src={punch ? anime.imageUrl : anime.punch}
                 alt={"Photo of " + anime.name}
             />
         </div>
     );
 };
-
-function MyButton() {
-    return (
-        <div>
-            <br />
-            <br />
-            <button>I'm a button</button>
-        </div>
-    );
-}
